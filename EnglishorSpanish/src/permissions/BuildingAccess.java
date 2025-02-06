6e42c49f5a0290d84750db2c93bbe7d0a4614fc1
WorawichY <152566081+WorawxxhOfficial@users.noreply.github.com>
2025-02-06 10:29:29 +0700

    Building access update

--- EnglishorSpanish/src/permissions/BuildingAccess.java
+++ EnglishorSpanish/src/permissions/BuildingAccess.java
@@ -1,4 +1,13 @@
 package permissions;
 
-public class BuildingAccess {
+public class BuildingAccess implements Permission {
+    @Override
+    public void grantPermission(String cardId, String target) {
+        System.out.println("Building access granted: Card " + cardId + " can access " + target);
+    }
+
+    @Override
+    public void revokePermission(String cardId, String target) {
+        System.out.println("Building access revoked: Card " + cardId + " cannot access " + target);
+    }
 }
