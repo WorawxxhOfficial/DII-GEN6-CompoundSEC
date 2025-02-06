64fb5f5087aeed2a483e2fac3de93347e6fbbd51
WorawichY <152566081+WorawxxhOfficial@users.noreply.github.com>
2025-02-06 10:40:47 +0700

    main update for UI

--- EnglishorSpanish/src/Main.java
+++ EnglishorSpanish/src/Main.java
@@ -1,9 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import
+import gui.KeyCardGUI;
+
+import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
-
+        
+        SwingUtilities.invokeLater(() -> {
+            KeyCardGUI gui = new KeyCardGUI();
+            gui.setVisible(true);
+        });
     }
 }
